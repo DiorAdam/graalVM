b60dc1a330ba09eb83193fa02ace7d133ddbe374
root <root@LAPTOP-084A0KKH.localdomain>
2021-04-28 21:42:44 +0200

    feat data and project architecture

---
+++ src/graalvm_env.sh
@@ -0,0 +1,3 @@
+export PATH=/mnt/d/k1gilo/Prog\ Tools/graalvm_lin/graalvm-ce-java11-21.1.0/bin:$PATH
+export JAVA_HOME=/mnt/d/k1gilo/Prog\ Tools/graalvm_lin/graalvm-ce-java11-21.1.0
+echo graalvm environment is ready
